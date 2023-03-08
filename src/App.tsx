71eb1d7954073a8d75b17f67be14f7fb46b36a2d
Antoine Deloy <antoine.deloy1@decathlon.com>
2023-03-08 15:30:36 +0100

    useStateDiveInit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
 import './App.css'
-import UseStateDive from "./useStateDive";
+import UseStateDive from "./useStateDiveComplete";
 
 function App() {
-
- let [value, setValue] = useState(0);
-
- function addOne() {
-  setValue(value++);
- }
 
   return (
     <div>
--- src/useStateDive/index.tsx
+++ src/useStateDive/index.tsx
@@ -1,59 +1,7 @@
-import { useState } from "react";
-
-function NameList() {
-	const [list, setList] = useState(["Jack", "John", "Jenny"]);
-	const [name, setName] = useState("");
-
-	const addNameToList = () => {
-		setList([...list, name]);
-		setName("");
-	}
-
-	return (
+const useStateDive = () => {
+	return(
 		<div>
-			<ul>
-				{ list.map((name) => (
-					<li key={name}>{name}</li>
-				))}
-			</ul>
-			<input
-			type="text"
-			value={name}
-			onChange={(e) => setName(e.target.value)}>
-			</input>
-			<button
-			onClick={addNameToList}>
-				Add name to List
-			</button>
+			Use State Dive
 		</div>
 	)
-}
-
-function Counter() {
-
-	let [value, setValue] = useState(0);
-
-	function addOne() {
-		setValue(value++);
-	}
-
-	return (
-		<div>
-			<button
-				onClick={addOne}> Count = {value}
-			</button>
-		</div>
-	);
-}
-
-function UseStateDive() {
-
-	return (
-		<div>
-			<Counter />
-			<NameList />
-		</div>
-	);
-}
-
-export default UseStateDive;
+};
